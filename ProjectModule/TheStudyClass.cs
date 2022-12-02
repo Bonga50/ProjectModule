63f36d92535bd292c65c365b1a889aa2d5972eb9
Siyabonga Zunguza <siyabonga4767@gmail.com>
2022-12-02 07:31:18 +0200

    Add project files.

---
+++ ProjectModule/TheStudyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectModule
+{
+    public static class TheStudyClass
+    {
+
+        public static double SelfStudy(int numberOfCreds,DateTime Startdate,int NumberOFWeeks,double HoursPerWeek) {
+
+            double selfStudyhrs;
+           
+            selfStudyhrs = (numberOfCreds * 10 / NumberOFWeeks) - HoursPerWeek;
+
+            return selfStudyhrs;
+        }
+
+        public static double TotalhrsPreMod(int numberOfCreds, DateTime Startdate, int NumberOFWeeks, double HoursPerWeek)
+        {
+
+            double TotalselfStudyhrs;
+            
+            double selfStudyhrs = (numberOfCreds * 10 / NumberOFWeeks) - HoursPerWeek;
+
+            TotalselfStudyhrs = NumberOFWeeks * selfStudyhrs;
+
+            return TotalselfStudyhrs;
+        }
+    }
+}
